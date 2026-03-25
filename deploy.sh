0dc9e447298b622973fbaeb289a160f448921bba
Andrew McWatters <andrew@andrewmcwatters.com>
2026-03-25 11:08:07 -0700

    Refactor deployment script and update sudoers for nginx-deploy

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,4 @@ GIT_BRANCH=${GIT_BRANCH:-main}
 git fetch origin "$GIT_BRANCH"
 git reset --hard "origin/$GIT_BRANCH"
 
-sudo cp etc/nginx/conf.d/*.conf /etc/nginx/conf.d/
-
-sudo nginx -t
-sudo systemctl reload nginx
-
-sudo certbot --nginx -n --agree-tos -m email@example.com --redirect -d example.com,www.example.com
+sudo nginx-deploy
